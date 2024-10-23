cfeebc00a0bf995257d38d71d7fccc0070e75311
elkamelsallemi <elkamel.sallemi@gmail.com>
2024-10-23 15:34:53 +0200

    Update

--- vars/varsPipelineTemplateLauncher.groovy
+++ vars/varsPipelineTemplateLauncher.groovy
@@ -3,21 +3,19 @@ import org.demoJenkins.common.IJenkinsSteps
 import org.demoJenkins.dto.InputDTO
 import org.demoJenkins.template.PipelineTemplate
 
-// Global InputDTO instance
-InputDTO inputDTO = new InputDTO()
 
-// Function to set values in the InputDTO instance
 def setInputDTOValues(Map params) {
+
+    InputDTO inputDTO = new InputDTO()
+
     inputDTO.name = params.name ?: 'defaultName'
     inputDTO.gitUrl = params.gitUrl ?: 'defaultUrl'
     inputDTO.extraParam = params.extraParam ?: 'defaultExtraParam'
-}
 
-def getInputDTO() {
     return inputDTO
 }
 
-def runPipelineTemplate(steps){
+def runPipelineTemplate(steps, InputDTO inputDTO){
     ContextRegistry.registerDefaultContext(steps)
     IJenkinsSteps stepExecutor = ContextRegistry.getContext().getStepExecutor()
     return new PipelineTemplate(stepExecutor).run(inputDTO)
