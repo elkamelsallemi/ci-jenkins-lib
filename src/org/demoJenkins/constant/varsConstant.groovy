4351dc1f43e4e7265a17dd40c11e9650388fc369
elkamelsallemi <elkamel.sallemi@gmail.com>
2024-10-30 10:15:38 +0100

    Update

---
+++ src/org/demoJenkins/constant/varsConstant.groovy
@@ -0,0 +1,7 @@
+package org.demoJenkins.constant
+
+class varsConstant {
+    static final String MY_CONSTANT = "This is a constant value"
+    static final int MAX_LIMIT = 100
+}
+
--- vars/varsConstant.groovy
+++
@@ -1,5 +0,0 @@
-
-MY_CONSTANT = "This is a constant value"
-MAX_LIMIT = 100
-
-
