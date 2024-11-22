f7c86aaf964e2253ed29296aeb14782d6f72a6ec
elkamelsallemi <elkamel.sallemi@gmail.com>
2024-11-22 12:25:08 +0100

    Update

--- src/org/demoJenkins/dto/InputDTO.groovy
+++ src/org/demoJenkins/dto/InputDTO.groovy
@@ -6,10 +6,6 @@ class InputDTO implements Serializable{
     String extraParam
 
     InputDTO(Map setInputValuesDTO){
-        setInputDTO(setInputValuesDTO)
-    }
-
-    def setInputDTO(Map setInputValuesDTO) {
         this.name = setInputValuesDTO.name ?: 'defaultName'
         this.gitUrl = setInputValuesDTO.gitUrl ?: 'defaultUrl'
         this.extraParam = setInputValuesDTO.extraParam ?: 'defaultExtraParam'
