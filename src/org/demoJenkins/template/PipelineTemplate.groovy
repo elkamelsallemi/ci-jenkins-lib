190266696c26c1b53b90424ffc7b04804ff09058
elkamelsallemi <elkamel.sallemi@gmail.com>
2024-12-02 15:51:25 +0100

    Update

--- src/org/demoJenkins/template/PipelineTemplate.groovy
+++ src/org/demoJenkins/template/PipelineTemplate.groovy
@@ -20,6 +20,7 @@ class PipelineTemplate extends BaseStage implements Serializable{
                 String version='0.0.dev1g824dc41d'
                 String PATH='resource/pkg_meta.py'
                 this._steps.echo('stage build Environment')
+                this._steps.sh("ls -l")
                 this._steps.sh("cat ${PATH}")
                 this._steps.sh("sed -ri 's/(^\\s*__version__\\s*=\\s*).*/\\1\\\"0.0.dev1g824dc41d\\\"/' ${PATH}")
             }
