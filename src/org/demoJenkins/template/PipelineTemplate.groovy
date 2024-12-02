1af235b93f57ebd0843e9e5f27896d5774ff8a1e
elkamelsallemi <elkamel.sallemi@gmail.com>
2024-12-02 16:10:17 +0100

    Update

--- src/org/demoJenkins/template/PipelineTemplate.groovy
+++ src/org/demoJenkins/template/PipelineTemplate.groovy
@@ -17,12 +17,19 @@ class PipelineTemplate extends BaseStage implements Serializable{
     void triggerFlow(InputDTO inputDTO){
         try {
             stage('build stage') {
-                String version='0.0.dev1g824dc41d'
-                String PATH='pkg_meta.py'
-                this._steps.echo('stage build Environment')
-                this._steps.sh("rm pkg_meta.py")
-                this._steps.sh '''echo "__version__ = '0.0.1'" > pkg_meta.py'''
-                this._steps.sh("""sed -ri 's/(^\\s*__version__\\s*=\\s*')[^']*(.*)/\\1\\'${version}'/' ${PATH}""")
+                String version = '0.0.dev1g824dc41d'
+                String PATH = 'pkg_meta.py'
+
+// Step 1: Remove existing file if it exists
+                this._steps.sh("rm -f ${PATH}")
+
+// Step 2: Create the file with the initial version
+                this._steps.sh("echo '__version__ = \"0.0.1\"' > ${PATH}")
+
+// Step 3: Use sed to update the version in the file
+                this._steps.sh("sed -ri 's/(^\\s*__version__\\s*=\\s*')[^']*(.*)/\\1\\'${version}'/' ${PATH}")
+
+// Step 4: Display the content of the file to verify
                 this._steps.sh("cat ${PATH}")
             }
         }catch (e) {
